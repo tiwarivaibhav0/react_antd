364be564bb26b314a2253c81453c8270f7570873
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-10-28 10:16:05 +0530

    expandable

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
 import { Table } from "antd";
 import "antd/dist/antd.css";
 import React, { useEffect, useState } from "react";
-const columns = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "Phone",
-    dataIndex: "phone",
-    key: "phone",
-  },
-  {
-    title: "Address",
-    dataIndex: "address",
-    key: "address",
-  },
-  {
-    title: "Action",
-    dataIndex: "",
-    key: "x",
-    render: () => <a>Delete</a>,
-  },
-];
 
 const App = () => {
   const [data, setData] = useState();
+  const deleteHandler = (event,record) => {
+    event.preventDefault();
+    console.log(event,record);
+    setData(data.filter(item=>item.key!==record.key))
+    // data.splice(record, 1);
+    // setData([...data]);
+  };
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Phone",
+      dataIndex: "phone",
+      key: "phone",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "Action",
+      dataIndex: "",
+      key: "x",
+      render: (text,record) => <a onClick={(event) => deleteHandler(event,record)}>Delete</a>,
+    },
+  ];
+
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
@@ -38,6 +46,7 @@ const App = () => {
             name: item.name,
             phone: item.phone,
             address: item.address.city,
+            description: item.email,
           };
           temp.push(row);
         });
@@ -47,28 +56,18 @@ const App = () => {
   return (
     <Table
       columns={columns}
-      onRow={(record, rowIndex) => {
-        return {
-          onClick: (event) => {
-            if (event.target.tagName === "A") {
-              data.splice(rowIndex, 1);
-              setData([...data]);
-            }
-          }, 
-        };
+      
+      expandable={{
+        expandedRowRender: (record) => (
+          <p
+            style={{
+              margin: 0,
+            }}
+          >
+            {record.description}
+          </p>
+        ),
       }}
-      // expandable={{
-      //   expandedRowRender: (record) => (
-      //     <p
-      //       style={{
-      //         margin: 0,
-      //       }}
-      //     >
-      //       {record.description}
-      //     </p>
-      //   ),
-      //   rowExpandable: (record) => record.name !== "Not Expandable",
-      // }}
       dataSource={data}
     />
   );
